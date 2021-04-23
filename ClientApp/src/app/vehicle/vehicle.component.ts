d01f0af64cf49079c65eb14037b9cd04baa83294
Pushpa Raj Dangi <dcpushparaj@gmail.com>
2021-04-23 16:55:45 +0545

    vehiclecomponent

--- ClientApp/src/app/vehicle/vehicle.component.ts
+++ ClientApp/src/app/vehicle/vehicle.component.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs-compat';
 import { SaveVehicle } from '../models/SaveVehicle';
 import { KeyValuePair } from '../models/KeyValuePair';
 import * as _ from 'underscore';
+import { Vehicle } from '../models/vehicle';
 
 @Component({
   selector: 'app-vehicle',
@@ -51,21 +52,23 @@ export class VehicleComponent implements OnInit {
     var sources = [
       this.vehicleService.getMakes(),
       this.vehicleService.getFeatures(),
-    ]
+    ];
+    if (this.vehicle.id)
+      sources.push(this.vehicleService.getVehicle(this.vehicle.id));
+
     Observable.forkJoin(sources).subscribe(data => {
       this.makes = data[0];
       this.features = data[1];
+
       if (this.vehicle.id) {
         this.setVehicle(data[2]);
         this.populateModels();
       }
-    },
-      error => {
-        if (error.status == 400) {
-          this.router.navigate["/home"];
-        }
-      }
-    );
+    }, err => {
+      if (err.status == 404)
+        this.router.navigate(['/home']);
+    });
+
     if (this.vehicle.id)
       sources.push(this.vehicleService.getVehicle(this.vehicle.id));
 
@@ -76,20 +79,23 @@ export class VehicleComponent implements OnInit {
   private setVehicle(v) {
     this.vehicle.id = v.id;
     this.vehicle.makeId = v.make.id;
-    this.vehicle.makeId = v.model.id;
+    this.vehicle.modelId = v.model.id;
     this.vehicle.isRegistered = v.isRegistered;
     this.vehicle.contact = v.contact;
-    this.vehicle.features = _.pluck(v.featurs, 'id');
+    this.vehicle.features = _.pluck(v.features, 'id');
   }
+
   onMakeChange() {
     this.populateModels();
-    delete this.vehicle.modelId;
+
+    delete this.vehicle.modelId
   }
 
   private populateModels() {
     var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
-    this.models = selectedMake ? selectedMake.models : "";
+    this.models = selectedMake ? selectedMake.models : [];
   }
+
   onFeatureToggle(featureId, $event) {
     if ($event.target.checked)
       this.vehicle.features.push(featureId);
